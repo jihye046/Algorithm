82f09afd3a418d11f0454ae4eb180e081509ccb9
jihye-Ha <140369193+jihye046@users.noreply.github.com>
2024-11-04 21:50:54 +0900

    [Bronze I] Title: 소인수분해, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		if(N == 1) {
+			sc.close();
+		}
+		
+		// 소인수 분해
+		for(int i = 2; i <= N; i++) {
+			while(N % i == 0) {
+				System.out.println(i);
+				N = N / i;
+			}
+			if(N == 1) break;
+		}	
+		
+	}
+}
